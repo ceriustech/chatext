5cb04fb707a154486323c83881d75b693076a9d6
Bilal Masters <ceriustech@gmail.com>
2023-08-21 22:10:52 -0500

    refactor: Submit file button is now properly placed

--- extension/client/src/components/Container/Container.js
+++ extension/client/src/components/Container/Container.js
@@ -3,9 +3,16 @@ import '../Buttons/SubmitFile/SubmitFile';
 
 class Container extends LitElement {
 	static styles = css`
+		.container {
+			position: absolute;
+			bottom: 15px;
+		}
+
 		@media screen and (max-width: 768px) {
 			.container {
 				padding: 0 12px;
+				position: absolute;
+				bottom: 10px;
 			}
 		}
 	`;
