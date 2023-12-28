e25a4a6917f2733e8bd4b060a19b04ab6ecf9cad
Bilal Masters <ceriustech@gmail.com>
2023-12-27 19:03:01 -0600

    fix: Hot fix for native file upload button position

--- extension/client/public/content.js
+++ extension/client/public/content.js
@@ -34,7 +34,11 @@
 		targetElement = document.querySelector(targetElementSelector);
 
 		if (fileUploadElement) {
+<<<<<<< Updated upstream
 			fileUploadElement.parentElement.parentElement.style.marginLeft = '30px';
+=======
+			fileUploadElement.style.marginLeft = '30px';
+>>>>>>> Stashed changes
 			targetElement.style.paddingLeft = '76px';
 		} else {
 			console.log('UNABLE TO DETECT ELEMENT');
