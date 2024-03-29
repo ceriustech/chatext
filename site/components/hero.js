646b77dea965aa6e019078856c50fc5b5b039e0b
Bilal Masters <ceriustech@gmail.com>
2024-03-28 19:16:56 -0500

    feat: Testing

--- site/components/hero.js
+++ site/components/hero.js
@@ -26,7 +26,7 @@ const Hero = () => {
 				<div className="flex items-center w-full lg:w-1/2">
 					<div className="max-w-2xl mb-8">
 						<h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-							ChatExt - AI File Uploader
+							123ChatExt - AI File Uploader
 						</h1>
 						<p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
 							ChatExt allows you to Upload files to chatGPT, up to 10 at a time.
